973838d211cce376a49fcf3e972b2f2e27248f0f
vasanthkrishnan <vasanthakrishnans22it@srishakthi.ac.in>
2024-10-05 17:40:57 +0530

    corrected-login-for-user-admin

--- user/src/pages/Login.jsx
+++ user/src/pages/Login.jsx
@@ -8,29 +8,30 @@ export const Login = () => {
 
   const handleFormSubmit = async (e) => {
     e.preventDefault()
+    const role = roleRef.current.value
+
+    if(role === "Select Role") {
+      console.log("Role is required")
+      return
+    }
     const loginData = {
       email : nameRef.current.value,
       password : passwordRef.current.value,
-      role : roleRef.current.value
+      role : role,
     }
     console.log(loginData.email)
     console.log(loginData.password)
     console.log(loginData.role)
     try {
-      const response = await fetch('http://localhost:5554/login', {
+      const response = await fetch('http://localhost:5555/login', {
         method : 'POST',
         headers : { 'Content-Type': 'application/json'},
         body : JSON.stringify(loginData)
       })
       if(response.ok) {
-      const data = await response.json()
-      if(data.role === 'admin') {
-        console.log("Admin login successful")
+        const data = await response.json()
+        console.log(`${data.role} login successful`)
       }
-      else if(data.role === 'student') {
-        console.log("Student login successful")
-      }
-    }
     else {
       const errorDate = await response.json()
       console.log("Login failed " + errorDate.message)
@@ -48,12 +49,12 @@ export const Login = () => {
                 </div>
                 <div className='h-[70%] w-full flex justify-center items-center'>
                   <form onSubmit={ handleFormSubmit } className='h-full w-[50%] flex flex-col mt-[3rem] gap-5 justify-start items-center'>
-                    <input ref={ nameRef } className='border-gray-500 border p-[.5rem] h-[2.5rem] w-[20rem]' type="text" name="" placeholder='Username' id="" />
-                    <input ref={ passwordRef  } className='border-gray-500 border p-[.5rem] h-[2.5rem] w-[20rem]' type="password" name="" placeholder='Password' id="" />
+                    <input ref={ nameRef } className='border-gray-500 border p-[.5rem] h-[2.5rem] w-[20rem]' type="text" name="" placeholder='Username' required />
+                    <input ref={ passwordRef  } className='border-gray-500 border p-[.5rem] h-[2.5rem] w-[20rem]' type="password" name="" placeholder='Password' required />
                     <select ref={ roleRef } className='border-gray-500 border p-[.5rem] h-[2.5rem] w-[20rem]'>
                       <option>Select Role</option>
-                      <option>Student</option>
-                      <option>Admin</option>
+                      <option value="student">Student</option>
+                      <option value="admin">Admin</option>
                     </select>
                     <button type='submit' className='p-[.5rem] h-[3rem] w-[20rem] bg-[#008000] text-white font-bold text-lg'>Log in</button>
                   </form>
