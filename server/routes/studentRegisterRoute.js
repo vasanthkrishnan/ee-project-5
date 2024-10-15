44fc573e96ea20c7a961647495aeef3e8d4aef97
vasanthkrishnan <vasanthakrishnans22it@srishakthi.ac.in>
2024-10-15 22:17:18 +0530

    put-get-delete

--- server/routes/studentRegisterRoute.js
+++ server/routes/studentRegisterRoute.js
@@ -13,4 +13,28 @@ router.post('/add', async (req, res) => {
     }
 })
 
+router.get('/all', async (req, res) => {
+    try {
+        const fetchedData = await Student.find()
+        res.status(200).json(fetchedData)
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
+})
+
+
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const currentData = await Student.findById({_id: id})
+        if(!currentData) {
+            res.status(500).json("No data Available")
+        }
+        const deleteData = await Student.findByIdAndDelete(id)
+        res.status(200).json(deleteData)
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+})
+
 module.exports = router
